318089ccc8eacc86c255d98292a12185b43cde06
Sindhu M N <sindhumn09@gmail.com>
2024-11-10 10:22:17 +0530

    DAY7

---
+++ strcy2strct.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+struct Adress{
+    char street[20];
+    char city[20];
+    char state[20];
+};
+strudt person{
+    int age;
+    char name[20];
+    struct Adress adress;
+};
+int main{
+    struct Person person
+
+    printf("Enter the name:");
+    scanf("%s, petson.name");
+    printf("Enter age:");
+    scanf("%d",&person.age);
+    printf("Enter street:");
+    scanf("%s",person adress.street);
+    printf("Enter city");
+    scanf("%s",person.address.state);
+
+    printf("\nName:%s\n",person.name);
+    printf("Age:%d\n",person.age);
+    printf("Street:%s\n",person.address.street);
+    printf("City")
+}
