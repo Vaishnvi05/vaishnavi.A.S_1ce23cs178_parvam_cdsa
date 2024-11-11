d679ce61700b5e75bd2b6e1c987151b478bdf313
Sindhu M N <sindhumn09@gmail.com>
2024-11-11 08:41:37 +0530

    DAY8

---
+++ Lower_case_string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main(){
+    char str[]="Hello,World!";
+    int i=0;
+
+    while (str[i]!='\0'){
+        if(str[i]>='A'&&str[i]<='z'){
+            str[i]=str[i]-'a'-'A';
+        }
+        i++;
+    }
+    printf("Lowercase string:%s\n",str);
+    return 0;
+ }
